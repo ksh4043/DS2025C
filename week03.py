bb2ffd7e8128e19710edbfda01b766938fb1f72b
ksh4043 <rlatlgus925@naver.com>
2025-03-24 15:18:29 +0900

    v1.0

--- week03.py
+++ week03.py
@@ -1,10 +1,9 @@
 def inters(l1, l2):
-    l3 = []
-    for v in l1:
-        if v in l2:
-            l3.append(v)
+    s1 = set(l1)
+    s2 = set(l2)
 
-    return l3
+    # return list(s1.intersection(s2))  set 타입 내부적으로 지원하는 중복값 찾기 메서드
+    return list(s1 & s2)    # set 타입은 & 키워드로 중복값 찾기 가능 // | 기호는 합집합 // - 기호로 차집합
 
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
