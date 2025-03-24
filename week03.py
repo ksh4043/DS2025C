fc274fd3d8939d666cc308dbec069a6c0a78ace2
ksh4043 <rlatlgus925@naver.com>
2025-03-24 14:24:04 +0900

    v0.5 array

--- week03.py
+++ week03.py
@@ -1,3 +1,18 @@
-l = [99, 0, -7, 0, 16]
-for i in range(len(l)):
-    print(f"{l[i]:2} {id(l[i])}")
+import array
+
+arr = array.array('i',[99, 0, -7, 0, 16])   # 키워드 'i'는 integer, 'f'면 float
+# for i in range(len(arr)):
+#     print(f"{arr[i]:2} {id(arr[i])}")
+
+def move_zeros(a_list):
+    zero_index = 0
+    for index, n in enumerate(a_list):
+        if n != 0:
+            a_list[zero_index] = n
+            if zero_index != index:
+                a_list[index] = 0
+            zero_index += 1
+    return a_list
+
+arr = move_zeros(arr)
+print(arr)
