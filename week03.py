2d0526e5ce07a993c5289f2f5265f79e9cf6aabd
ksh4043 <rlatlgus925@naver.com>
2025-03-24 14:56:49 +0900

    v0.7

--- week03.py
+++ week03.py
@@ -1,6 +1,3 @@
-groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
-# ratings = [1, 2, 4, 3, 12]
-ratings = [1, 2, 4, 3]
-
-group_rating = list(zip(groups, ratings))   # zip -> 각 list의 대응 되는 인덱스 두개를 tuple 형태로 리턴. len 값이 작은 list 기준
-print(group_rating)
+cities = ['Suwon', 'Hwasung', 'Incheon', 'Incheon', 'Bucheon', 'Seoul']
+cities = set(cities)
+print(cities)
