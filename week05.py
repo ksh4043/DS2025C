562e982c4326064f163beb20f63e9cf209a2604f
ksh4043 <rlatlgus925@naver.com>
2025-04-14 14:25:06 +0900

    v2.2 modify Code

--- week05.py
+++ week05.py
@@ -1,9 +1,9 @@
 import copy
 
 class Node:
-    def __init__(self,data,next=None):
+    def __init__(self,data,link=None):
         self.data = data
-        self.next = next
+        self.link = link
 
 
     def __str__(self):
@@ -14,57 +14,54 @@ class Queue:
     def __init__(self):
         self.front = None
         self.rear = None
-        self._size = 0
+        self.size = 0
+        # _size : _ 키워드로 히든 필드 네임을 지정 <> private 과는 다른 개념
 
 
     def enqueue(self, item):
-        self._size += 1
+        self.size += 1
         node = Node(item)
         if self.rear is None:
             self.front = node
             self.rear = node
         else:
-            self.rear.next = node
+            self.rear.link = node
             self.rear = node
 
 
     def dequeue(self):
         if self.front is None:
             return "This Queue is Empty"
-        self._size -= 1
+        self.size -= 1
         temp = self.front
-        self.front = self.front.next
+        self.front = self.front.link
         if self.front is None:
             self.rear = None
         return temp.data
 
 
-    def size(self):
-        return self._size
-
-
     def __str__(self):
-        if self.front is None:
+        if self.rear is None:
             return "This Queue is Empty"
         for_return_list = []
         current = copy.deepcopy(self.front)
         while current:
             for_return_list.append(str(current))
-            current = current.next
+            current = current.link
 
         return f"{for_return_list}"
 
 
 q1 = Queue()
-q2 = Queue()
 q1.enqueue(55)
 q1.enqueue(5)
 q1.enqueue(555)
 q1.enqueue(5555)
 
-print(q1)
-print(q2)
-print(q1.size())
-
+print(q1.size, q1)
+print(q1.dequeue())
+print(q1.size, q1)
 q1.enqueue(100)
-print(q1)
+print(q1.size, q1)
+q1.enqueue("DataBase")
+print(q1.size, q1)
