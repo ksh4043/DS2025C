ea7e742eb5f9b8f9f949ade16df79a30432adc1e
ksh4043 <rlatlgus925@naver.com>
2025-04-28 15:41:18 +0900

    v2.6 Tree

--- week08.py
+++ week08.py
@@ -2,7 +2,7 @@ def post_order(node):
     if node:
         post_order(node.left)
         post_order(node.right)
-        print(node.data, end='-')
+        print(node.data, end='->')
 
 
 class TreeNode:
@@ -11,31 +11,42 @@ class TreeNode:
         self.data = None
         self.right = None
 
+
+def insert(root, value):
+    new_node = TreeNode()
+    new_node.data = value
+
+    if root is None:
+        return new_node
+
+    node = TreeNode()
+    node.data = value
+    current = root
+
+    while True:
+        if value < current.data:
+            if current.left is None:
+                current.left = node
+                break
+            current = current.left
+        else:
+            if current.right is None:
+                current.right = node
+                break
+            current = current.right
+
+    return root
+
+
+# def search(value):
+
+
 if __name__ == "__main__":
     numbers = [10, 15, 8, 3, 9]
     root = None
 
-    node = TreeNode()
-    node.data = numbers[0]
-    root = node
-
-    #2번째 원소부터
-    for number in numbers[1:]:
-        node = TreeNode()
-        node.data = number
-        current = root
-
-        while True:
-            if number < current.data:
-                if current.left is None:
-                    current.left = node
-                    break
-                current = current.left
-            else:
-                if current.right is None:
-                    current.right = node
-                    break
-                current = current.right
+    for number in numbers:
+        root = insert(root, number)
 
     print("BST 구성 완료")
     post_order(root)
